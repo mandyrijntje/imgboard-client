7ce8982a378e00395540cc815ced56025cec7613
messmonte <amandamariamonte@gmail.com>
2020-03-15 12:42:45 +0100

    added routes to App, created user and images actions

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,25 +1,62 @@
-const initialState = {
-    auth: "",
-    createdUser: "",
-    list: [],
-    userDisplayed: null
-  };
-  
-  export default (state = initialState, { type, payload }) => {
-    switch (type) {
-      case "GET_JWT":
-        return { ...state, auth: payload };
-      case "NEW_USER":
-        return { ...state, createdUser: payload };
-      case "FETCH_USERS":
-        return { ...state, list: payload };
-      case "FETCH_UNIQUE_USER":
-        return {
-          ...state,
-          userDisplayed: payload
-        };
-      default:
-        return state;
-    }
-  };
-  
+import request from "superagent";
+
+// const baseUrl = "https://warm-dusk-67488.herokuapp.com";
+const baseUrl = "http://localhost:4000";
+
+export const getJwt = jwt => ({
+  type: "GET_JWT",
+  payload: jwt
+});
+
+export const newUser = payload => ({
+  type: "NEW_USER",
+  payload
+});
+
+export const fetchUsers = users => ({
+  type: "FETCH_USERS",
+  payload: users
+});
+
+export const fetchUniqueUser = user => ({
+  type: "FETCH_UNIQUE_USER",
+  payload: user
+});
+
+export const login = (mail, pass) => dispatch => {
+  request
+    .post(`${baseUrl}/login`)
+    .send({ email: mail, password: pass })
+    .then(response => {
+      const action = getJwt(response.body.jwt);
+      dispatch(action);
+    })
+    .catch(error => console.log(error));
+};
+
+export const createUser = data => dispatch => {
+  request
+    .post(`${baseUrl}/user`)
+    .send(data)
+    .then(response => {
+      const action = newUser(response.body.email);
+      dispatch(action);
+    });
+};
+
+export const getUsers = () => dispatch => {
+  request
+    .get(`${baseUrl}/users`)
+    .then(response => {
+      const action = fetchUsers(response.body);
+      dispatch(action);
+    })
+    .catch(console.error);
+};
+
+export const getUniqueUser = id => dispatch => {
+  request.get(`${baseUrl}/users/${id}`).then(response => {
+    const action = fetchUniqueUser(response.body);
+    dispatch(action);
+  });
+};
