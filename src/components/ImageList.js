c2139d0c386eebc7d9b667d5f056b3fe886b4d18
messmonte <amandamariamonte@gmail.com>
2020-03-15 17:51:15 +0100

    sorting images by date

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -9,27 +9,41 @@ export default class ImageList extends Component {
     } else {
       return (
         <div>
-          {this.props.images.map(image => {
-            const dateCreation = new Date(image.createdAt);
-            const now = new Date();
-            const differenceHours = parseInt(
-              dateCreation.getHours() - now.getHours() / 3600000
-            );
-            return (
-              <div key={image.id}>
-                <h3>{image.title}</h3>
-                <img src={image.url} alt={image.title} />
-                <p>Uploaded {differenceHours} hours ago</p>
-                {currentPath.includes("users") ? (
-                  <EditFormContainer imageId={image.id} />
-                ) : null}
+          {this.props.images
+            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            .map(image => {
+              const dateCreation = new Date(image.createdAt);
 
-                <button onClick={() => this.props.destroyImage(image.id)}>
-                  Delete
-                </button>
-              </div>
-            );
-          })}
+              const now = new Date();
+
+              const differenceHours = parseInt(
+                now.getTime() - dateCreation.getTime()
+              );
+
+              console.log(now);
+              console.log(dateCreation);
+              console.log(differenceHours);
+              return (
+                <div key={image.id}>
+                  <h3>{image.title}</h3>
+                  <img src={image.url} alt={image.title} />
+                  <p>
+                    Uploaded{" "}
+                    {differenceHours > 3600000
+                      ? parseInt(differenceHours / 3600000)
+                      : parseInt(differenceHours / 60000)}{" "}
+                    {differenceHours > 3600000 ? "hours" : "minutes"} ago
+                  </p>
+                  {currentPath.includes("users") ? (
+                    <EditFormContainer imageId={image.id} />
+                  ) : null}
+
+                  <button onClick={() => this.props.destroyImage(image.id)}>
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
         </div>
       );
     }
