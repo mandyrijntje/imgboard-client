4ce047d2f96852f3e9617e73101da49e1a34b550
messmonte <amandamariamonte@gmail.com>
2020-03-15 14:44:28 +0100

    testing delete button

--- src/store/reducers/images.js
+++ src/store/reducers/images.js
@@ -10,7 +10,10 @@ export default (state = initialState, { type, payload }) => {
     case "NEW_IMAGE":
       return { ...state, list: [payload, ...state.list] };
     case "REMOVE_IMAGE":
-      return { ...state, list: state.filter(image => image.id !== payload) };
+      return {
+        ...state,
+        list: state.list.filter(image => image.id !== payload)
+      };
     case "IMAGES_BY_USER":
       return { ...state, listByUser: payload };
     case "UPDATE_IMAGE":
