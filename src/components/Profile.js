ab8eed1203cb6eb2de888efabd7a0fe2fe52def2
messmonte <amandamariamonte@gmail.com>
2020-03-15 22:45:12 +0100

    profile container modified

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import ImageList from "./ImageList";
 
 export default class Profile extends Component {
-  componentWillUnmount(){
-   this.props.user.name=""
-  }
+ 
   render() {
     return (
       <div>
