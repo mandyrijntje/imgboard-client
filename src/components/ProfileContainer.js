d7f37711c9fb2b78ce8eba4f436377564c9206a3
messmonte <amandamariamonte@gmail.com>
2020-03-15 12:46:03 +0100

    Added userslistcontainer and profilecontainer

---
+++ src/components/ProfileContainer.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getUniqueUser } from "../store/actions/user";
+import { getUserImages } from "../store/actions/images";
+import Profile from "./Profile";
+
+class ProfileContainer extends Component {
+  componentDidMount() {
+    this.props.getUserImages(this.props.match.params.id);
+  }
+
+  render() {
+    const userDisplayed = this.props.user.userDisplayed;
+
+    if (!userDisplayed) {
+      return <p>loading</p>;
+    } else {
+      return <Profile user={userDisplayed} images={this.props.images} />;
+    }
+  }
+}
+
+const mapStateToProps = state => ({
+  user: state.user,
+  images: state.images.listByUser
+});
+
+const mapDispatchToProps = {
+  getUniqueUser,
+  getUserImages
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileContainer);
